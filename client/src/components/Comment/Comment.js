1252fda65f5903270d3f039f3ef4a1262637b046
GalyaIT <givanova76@gmail.com>
2021-04-25 17:28:00 +0300

    install moment library, add Comment component

---
+++ client/src/components/Comment/Comment.js
@@ -0,0 +1,22 @@
+import Moment from 'react-moment';
+import './_Comment.scss'
+const Comment = ({ content, creator, createdAt }) => {
+ 
+    return (
+
+
+        <div className="comment-wrapper  ">          
+            <section className="comment_content ">
+                 <p >{content}</p>
+            </section>
+            <section className="comment_info ">
+                   <p>posted by @{creator}</p>
+           <p>at <Moment format="DD MMM YYYY HH:mm:ss">{createdAt}</Moment></p>
+            </section>
+        
+          
+
+        </div>
+    )
+}
+export default Comment
